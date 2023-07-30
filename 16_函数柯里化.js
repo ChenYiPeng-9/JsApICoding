8382a3b412e022f7afedc1682121e1454b77c54d
cyp <885200359@qq.com>
2023-07-30 14:04:43 +0800

    柯里化

---
+++ 16_函数柯里化.js
@@ -0,0 +1,40 @@
+// 柯里化一般形式
+
+function curry(fn, ...outArgs) {
+  return function (...inArgs) {
+    const args = [...outArgs, ...inArgs];
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return curry(fn, ...args);
+    }
+  };
+}
+
+const curryTest = (a, b, c) => {
+  return a + b + c;
+};
+
+console.log(curryTest(1, 2, 3, 4, 5), "柯里化一般形式1");
+
+console.log(curry(curryTest, 1)(3)(2), "柯里化一般形式2");
+
+// 柯里化实现累加
+
+function curryAdd(...outArgs) {
+  const argsArr = [...outArgs];
+  let s = function (...inArgs) {
+    argsArr.push(...inArgs);
+    return s;
+  };
+
+  s.getSum = function () {
+    return argsArr.reduce((pre, cur) => {
+      return pre + cur;
+    }, 0);
+  };
+
+  return s;
+}
+
+console.log(curryAdd(1, 2, 3)(4, 5)(6).getSum(), "柯里化叠加");
